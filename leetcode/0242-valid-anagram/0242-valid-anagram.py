3009c49a4474a4156ae1e43299cea619af97aeea
lenchofekadu <lencho.fekadu@a2sv.org>
2026-03-25 19:36:38 +0300

    Add solution for Valid Anagram

--- leetcode/0242-valid-anagram/0242-valid-anagram.py
+++ leetcode/0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        t = sorted(t)
-        s = sorted(s)
-        print(t)
-        print(s)
-        if t in s or t == s:
-            return True
+        countT = Counter(t)
+        countS = Counter(s)
 
-        return False
+        return countS == countT
