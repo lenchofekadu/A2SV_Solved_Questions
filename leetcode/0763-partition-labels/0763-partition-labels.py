cc9be0403817d928ba93ca84919e8d9c290eb38a
lenchofekadu <lencho.fekadu@a2sv.org>
2026-03-26 18:08:36 +0300

    Add solution for Partition Labels

--- leetcode/0763-partition-labels/0763-partition-labels.py
+++ leetcode/0763-partition-labels/0763-partition-labels.py
@@ -1,23 +1,13 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        
-        last = {}
-
-        seen = set()
-
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] not in seen:
-                last[s[i]] = i 
-            seen.add(s[i])
-
+        last = {ch: i for i, ch in enumerate(s)}  
         res = []
-        size = 1
-        end = 0 
-        for idx, ch in enumerate(s):
-            end = max(last[ch], end)
-            if idx == end:
-                res.append(size)
-                size = 0 
+        left = 0
+        right = 0
         
-            size += 1
+        for i, ch in enumerate(s):
+            right = max(right, last[ch])  
+            if i == right:  
+                res.append(right - left + 1)
+                left = i + 1
         return res
