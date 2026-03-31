f120e5c0b184fb5a3f9347e89adb3996131d2754
lenchofekadu <lencho.fekadu@a2sv.org>
2026-03-31 15:26:25 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -2,14 +2,15 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         lst = [x for x in range(1, n + 1)]
-        print(lst)
         def subarray(arr, i, curr):
-            if i == len(arr):
+            if len(curr) == k:
                 res.append(curr[:])
+                return 
+            if i == len(arr):
                 return 
 
             subarray(arr, i + 1, curr + [arr[i]])
             subarray(arr, i + 1, curr)
         
         subarray(lst, 0, [])
-        return [x for x in res if len(x) == k]
+        return res
