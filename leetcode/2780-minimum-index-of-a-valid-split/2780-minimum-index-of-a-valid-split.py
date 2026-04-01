be9c024f1dfe27f1237ab227a983cf34f9888102
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-01 20:46:35 +0300

    Add solution for Minimum Index of a Valid Split

--- leetcode/2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ leetcode/2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -2,24 +2,23 @@ class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         
         count = Counter(nums)
-        freq = {}
-        for key, value in count.items():
-            freq[value] = key
         large = 0 
-        for value in count.values():
-            large = max(large, value)
-        r_dominant = large
-        large = freq[large]
+        for key, value in count.items():
+            if value > large:
+                large = value
+                dominant = key
 
-        left = []
+        r_dominant = large
         l_dominant = 0
         
         for idx, num in enumerate(nums):
-            left.append(num)
-            if num == large:
+           
+            if num == dominant:
                 l_dominant += 1
                 r_dominant -= 1
-            right = len(nums) - len(left)
-            if l_dominant > len(left) - l_dominant and r_dominant > right - r_dominant:
+            left = idx + 1
+            right = len(nums) - left
+            
+            if l_dominant > left // 2 and r_dominant > right // 2:
                 return idx
         return -1
