de73d79b20096f65a8b1ccc80e7bcb75827ca434
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-01 18:43:54 +0300

    Add solution for Largest Number

---
+++ leetcode/0179-largest-number/0179-largest-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(x) for x in nums]
+        def bubble_sort(arr):
+            n = len(arr)
+            for i in range(n):
+                for j in range(0, n - i - 1):
+                    first = arr[j] + arr[j + 1]
+                    second = arr[j + 1] + arr[j]
+                    if int(first) > int(second):
+                        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+ 
+            return arr
+        res = reversed(bubble_sort(nums))
+        return str(int("".join(res)))
+                    
+        
