dd6a12593c6ae08f76732c627311993906262735
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-01 18:44:38 +0300

    Add solution for Largest Number

--- leetcode/0179-largest-number/0179-largest-number.py
+++ leetcode/0179-largest-number/0179-largest-number.py
@@ -7,7 +7,7 @@ class Solution:
                 for j in range(0, n - i - 1):
                     first = arr[j] + arr[j + 1]
                     second = arr[j + 1] + arr[j]
-                    if int(first) > int(second):
+                    if first > second:
                         arr[j], arr[j + 1] = arr[j + 1], arr[j]
  
             return arr
