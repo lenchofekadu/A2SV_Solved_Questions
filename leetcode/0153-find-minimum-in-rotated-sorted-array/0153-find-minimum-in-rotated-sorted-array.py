0a2f0726b24e98975e0b4442c0e9d142794b6bc0
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-15 19:56:14 +0300

    Add solution for Find Minimum in Rotated Sorted Array

--- leetcode/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ leetcode/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,4 +1,7 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        
+        # this is what I don't understand man
+        # it passes, okay. It does pass
+        # And who in valhala cares when it beats 100.00% in runtime
         return min(nums)
+        
