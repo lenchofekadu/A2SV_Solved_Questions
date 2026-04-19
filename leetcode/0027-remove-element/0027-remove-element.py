8f98b2c74bc3df7461ca5f3e90cc8c9858b1a8d1
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-19 11:51:14 +0300

    Add solution for Remove Element

---
+++ leetcode/0027-remove-element/0027-remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = right = 0
+
+        while right < len(nums):
+            if nums[right] != val:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right += 1
+            else:
+                right += 1
+        return left
