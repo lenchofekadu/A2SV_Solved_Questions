52c7453bdb9ef9a582da5ffd5e05c58b1e1b88e1
lenchofekadu <lencho.fekadu@a2sv.org>
2026-04-19 12:11:15 +0300

    Add solution for Design Parking System

--- leetcode/1603-design-parking-system/1603-design-parking-system.py
+++ leetcode/1603-design-parking-system/1603-design-parking-system.py
@@ -1,28 +1,14 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
+
+        self.dict = {1:big, 2:medium, 3:small}
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big:
-                self.big -= 1
-                return True
-            return False
-        if carType == 2:
-            if self.medium:
-                self.medium -= 1
-                return True
-            return False
-        if carType == 3:
-            if self.small:
-                self.small -= 1
-                return True
-            return False
-            
-
+        if self.dict[carType]:
+            self.dict[carType] -= 1
+            return True
+        return False
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
