cb44013f7b22d63abf79641983a86b923e52ba32
lenchofekadu <lencho.fekadu@a2sv.org>
2026-06-02 13:15:12 +0300

    Add solution for Divide Players Into Teams of Equal Skill

--- leetcode/2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ leetcode/2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,22 +1,17 @@
-class Solution(object):
-    def dividePlayers(self, skill):
-        """
-        :type skill: List[int]
-        :rtype: int
-        """
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+       
         skill.sort()
-        left = 0
+        left = 0 
         right = len(skill) - 1
         check = skill[0] + skill[-1]
-        res = 0 
+        ans = 0
         while left < right:
-            
             if skill[left] + skill[right] != check:
-                return -1
-
-            res += skill[left] * skill[right]
-
+                ans = -1
+                break
+            else:
+                ans += skill[left] * skill[right]
             left += 1
             right -= 1
-        
-        return res
+        return ans
